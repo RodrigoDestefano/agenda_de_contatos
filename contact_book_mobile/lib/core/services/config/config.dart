39f1476f8307c45ff9d6c8d242da73873363b993
Rodrigo <rodrigo.destefano.santos@gmail.com>
2021-06-14 17:08:07 -0300

    Fix

--- contact_book_mobile/lib/core/services/config/config.dart
+++ contact_book_mobile/lib/core/services/config/config.dart
@@ -2,4 +2,4 @@
 // How has just one url and port an unique variable is necessary
 
 // localhost throws an exception error, so you need to use your IPv4 instead
-const String path = "http://192.168.0.2:8000";
+const String path = "http://IPv4:8000";
